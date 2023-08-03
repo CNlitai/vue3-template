6c036a86d0511d31bc37cc71c2ce759b3faf90c3
CN-litai <2083645366@qq.com>
2023-08-03 14:23:11 +0800

    Initial commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useUserStore } from '@/store/modules/user'
+import Admin from './components/Admin.vue'
+import Editor from './components/Editor.vue'
+const userStore = useUserStore()
+const isAdmin = userStore.roles.includes('admin')
+const env = import.meta.env
+</script>
+
+<template>
+	{{ env }}
+	<component :is="isAdmin ? Admin : Editor" />
+</template>
